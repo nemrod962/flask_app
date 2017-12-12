138f27433bc51209305ac1fa92d41583d10a9101
pablopenna <p.pena@edu.uah.es>
2017-12-12 14:13:46 +0100

    Modificado script de envio. NO requiere parametros pero no es flexible.

--- send_nc.sh
+++ send_nc.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #host -> 	172.16.199.1
 #vm -> 		172.16.199.128
-cat $1 | nc -l -p 1234
+#cat $1 | nc -l -p 1234
+tar -ac ./myfiles -f myflask.tar
+cat myflask.tar | nc -l -p 1234
+rm myflask.tar
