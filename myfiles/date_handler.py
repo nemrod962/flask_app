8db5637e8cf0094b8717bd898fd761ec581257b2
pablopenna <p.pena@edu.uah.es>
2017-12-14 12:35:39 +0100

    Se ha cambiado el manejo de MS. Ahora al obtener la fecha en ms no hay decimales. Tambień al pasar de ms a formato de fecha los segundos no muestran decimales.

--- myfiles/date_handler.py
+++ myfiles/date_handler.py
@@ -13,10 +13,16 @@ def getTime():
     hora = time.strftime("%H:%M:%S")
     return hora
 
+#time.time() deuvuelve el tiempo actual en microsegundos.
+#como queremos milisegundos, nos quedaran 3 cifras decimales.
+#utilizaremos int.round() para quitar los decimales
 def getDatetimeMs():
-    return time.time() * 1000.0
+    timeMs = int(round(time.time() * 1000.0))
+    return timeMs
 
 def msToDatetime(tiempoMs):
-    fechabuena = datetime.datetime.fromtimestamp(tiempoMs/1000.0)
+    #Redondeo para que los segundos no tengan decimales
+    fechaSegundos = int(round(tiempoMs/1000.0))
+    fechabuena = datetime.datetime.fromtimestamp(fechaSegundos)
     return fechabuena
 
