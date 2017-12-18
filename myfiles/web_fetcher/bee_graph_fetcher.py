bc71ab236d160d63fe0d2b6e9932288882b3c9ef
pablopenna <p.pena@edu.uah.es>
2017-12-18 12:53:51 +0100

    librería con una única función que devuleve el código html de las gráficas en beebotte. Se ha utilizado PhantomJS junto con Selenium para ejecutar el código Javascript en Beebotte que produce las gráficas.

--- myfiles/web_fetcher/bee_graph_fetcher.py
+++ myfiles/web_fetcher/bee_graph_fetcher.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import urllib2
 #import requests
 
@@ -39,22 +40,42 @@ import requests
 res = soup.find(id="numeros_generados")
 print res
 """
+def getGraphHTML(debug=False):
+    if debug:
+        print "GO!"
+    url = "https://beebotte.com/dash/"
+    tablaID = "09db3e70-df3a-11e7-bfef-6f68fef5ca14"
+    fullurl = url+tablaID
+    if debug:
+        print fullurl
+    """
+    response = urllib2.urlopen(fullurl)
+    page_source = response.read()
 
-print "GO!"
-url = "https://beebotte.com/dash/"
-tablaID = "09db3e70-df3a-11e7-bfef-6f68fef5ca14"
-fullurl = url+tablaID
-print fullurl
-response = urllib2.urlopen(fullurl)
-page_source = response.read()
+    soup = BeautifulSoup(page_source, "html.parser")
 
-soup = BeautifulSoup(page_source, "html.parser")
+    #print soup.prettify()
+    asd = tablaID+"_body"
+    print asd
+    res = soup.find_all(id=asd)
+    print res
+    """
 
-#print soup.prettify()
-asd = tablaID+"_body"
-print asd
-res = soup.find_all(id=asd)
-print res
+
+    driver = webdriver.PhantomJS()
+    driver.get(fullurl)
+    #page_source fetches page after rendering is complete
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    widgetsID = tablaID+"_body"
+    if debug:
+        print widgetsID
+    res = soup.find_all(id=widgetsID)
+    if debug:
+        print res
+    driver.save_screenshot('screen.png') # save a screenshot to disk
+    #Finalizo el driver
+    driver.quit()
+    return res
 """
 for link in soup.find_all('a'):
     print(link.get('href'))
