5022e9b2ac934613d8a1f006ab2d31b7823433b3
pablopenna <p.pena@edu.uah.es>
2017-12-22 10:40:07 +0100

    Cambiado el script de envio al host.

--- send_nc.sh
+++ send_nc.sh
@@ -2,6 +2,6 @@
 #host -> 	172.16.199.1
 #vm -> 		172.16.199.128
 #cat $1 | nc -l -p 1234
-tar -ac ./myfiles -f myflask.tar
+tar -ac ./src -f myflask.tar
 cat myflask.tar | nc -l -p 1234
 rm myflask.tar
