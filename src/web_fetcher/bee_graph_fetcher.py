1f4ebd5d603d7eb0442bebf109b3b6ab8beb1739
pablopenna <p.pena@edu.uah.es>
2017-12-22 10:38:01 +0100

    Se ha limpiado el archivo web_fetcher/bee_graph_fetcher de código comentado.
    Se han movido ficheros html que se han empleado en la fase de prueba u¡y que no son relevantes para la ejecución del programa a la carpeta pruebas.

--- src/web_fetcher/bee_graph_fetcher.py
+++ src/web_fetcher/bee_graph_fetcher.py
@@ -1,45 +1,20 @@
+# -*- coding: UTF-8 -*-
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import urllib2
-#import requests
 
-
-"""
-url = raw_input("Enter a website to extract the URL's from: ")
-r  = requests.get("http://" +url)
-data = r.text
-"""
-
-
-html_doc = """
-<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
-
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
-
-<p class="story">...</p>
-"""
-
-"""
-#get source
-response = urllib2.urlopen("http://www.numeroalazar.com.ar/")
-page_source = response.read()
-#print page_source
-
-print "SOUP!"
-soup = BeautifulSoup(page_source, "html.parser")
-
-print soup.prettify()
-
-import requests
-res = soup.find(id="numeros_generados")
-print res
-"""
+#Accede a la dirección donde se encuentra
+#el dashboard de Beebotte que he creado
+#donde se encuentran las gráficas de la 
+#variable que se almacena alli.
+#Abro esa dirección con phantomJS para
+#ejecutar el código de javascript que 
+#genera las gráficas. Una vez que
+#disponemos de todo el código html de 
+#la página, gráficas incluidas, nos
+#quedamos únicamente con las gráficas
+#gracias a BeautifulSoup.
+#Retornamos el código html de las gráficas
 def getGraphHTML(debug=False):
     if debug:
         print "GO!"
@@ -48,19 +23,6 @@ def getGraphHTML(debug=False):
     fullurl = url+tablaID
     if debug:
         print fullurl
-    """
-    response = urllib2.urlopen(fullurl)
-    page_source = response.read()
-
-    soup = BeautifulSoup(page_source, "html.parser")
-
-    #print soup.prettify()
-    asd = tablaID+"_body"
-    print asd
-    res = soup.find_all(id=asd)
-    print res
-    """
-
 
     driver = webdriver.PhantomJS()
     driver.get(fullurl)
@@ -76,18 +38,3 @@ def getGraphHTML(debug=False):
     #Finalizo el driver
     driver.quit()
     return res
-"""
-for link in soup.find_all('a'):
-    print(link.get('href'))
-"""
-
-"""
-from selenium import webdriver
-
-driver = webdriver.PhantomJS()
-driver.get('http://jokes.cc.com/')
-soupFromJokesCC = BeautifulSoup(driver.page_source) #page_source fetches page after rendering is complete
-driver.save_screenshot('screen.png') # save a screenshot to disk
-
-driver.quit()
-"""
