2e5b6c7294fdeaf38d2218c9428668793e6b37cb
pablopenna <p.pena@edu.uah.es>
2018-04-27 20:39:40 +0200

    Prueba flask con un template para probar OAuth2 de Google en javascript.

--- src/pruebas/javascript-flask/main.py
+++ src/pruebas/javascript-flask/main.py
@@ -5,9 +5,12 @@ from flask import Flask, url_for, request, render_template, redirect
 app = Flask(__name__) 
 
 #Functions for different routes in the server
-@app.route("/") 
+@app.route("/index") 
 def index():
     return render_template("index.html")
+@app.route("/") 
+def test():
+    return render_template("ejemplo.html")
 
 
 """
@@ -26,6 +29,6 @@ print(url3)
 
 #LAUNCH APP
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=5000)
 
 
