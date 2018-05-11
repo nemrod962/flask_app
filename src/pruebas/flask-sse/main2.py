f2e9c0ae23a1f48838d813b0298ebb76868ad9b8
Pablo <p.pena@edu.uah.es>
2018-05-11 16:39:36 +0200

    rpuebas/flask-sse/main2.py: Modificada la prueba de flask-sse de forma que solo le llega notificaciones del servidor al cliente cuando envía un texto al servidor diferente al anterior.
    Cuando se pulsa el botón de la web, se envía al servidor el texto que hay en el campo de texto a la derecha del botón. Si el texto enviado es diferente al que se ha enviado previamente, aparecerá una nueva línea en la página indicando la hora actual. Esto es un evento enviado del servidor.

--- src/pruebas/flask-sse/main2.py
+++ src/pruebas/flask-sse/main2.py
@@ -12,12 +12,13 @@ index_html = """
 </head>
 <body>
     <h1>hello, world</h1>
-    <ul id="events"></ul>
     <button id="boton" onclick="sendmsg();">Gen</button>
+    <input id="texto" type="text"></input>
+    <ul id="events"></ul>
     <script>
     
     window.onload = function() {
-        var source = new EventSource("/stream2");
+        var source = new EventSource("/stream3");
         source.onmessage = function(e) {
 
           var newElement = document.createElement("li");
@@ -31,25 +32,49 @@ index_html = """
     function sendmsg()
     {
         console.log("hola buenos dias");
+        var txt = document.getElementById('texto').value;
+        console.log("texot: " + txt);
         var xhr = new XMLHttpRequest();
         var loc = window.location.href
         console.log('loc: ' + loc)
-        loc += 'stream2'
+        loc += 'peticion'
         console.log('loc: ' + loc)
-        xhr.open('GET', loc);
+        xhr.open('POST', loc);
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xhr.onload = function() {
             //respuesta servidor
             resp = xhr.responseText;
             console.log('RESPONSE FROM SERVER: ' + resp);
         };
-        xhr.send('id=1');
+        xhr.send('txt='+txt);
         console.log('mensaje enviado');
     }
     </script>
 </body>
 </html>
 """
+
+varGlobal = ""
+varTemp = ""
+
+@app.route('/peticion', methods=['GET','POST'])
+def peticion():
+    if request.method=='POST':
+        var = request.form.get('txt','None')
+        print "SERVER - recibido: " + var
+        global varGlobal
+        varGlobal = var
+        return "soy /peticion"
+
+def streamer():
+    while True:
+        sleep(1)
+        data = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
+        yield 'data: {}\n\n'.format(data)
+
+@app.route('/stream')
+def stream():
+    return Response(streamer(), mimetype="text/event-stream")
 
 def streamer2():
     data = "Me has llamado a las: " + datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
@@ -58,11 +83,24 @@ def streamer2():
 
 @app.route('/stream2', methods=['GET','POST'])
 def stream2():
-    if request.method=='GET':
-        return Response(streamer2(), mimetype="text/event-stream")
-    else:
-        return "no"
+    return Response(streamer2(), mimetype="text/event-stream")
 
+def streamer3():
+    global varGlobal
+    global varTemp
+    #varTemp = varGlobal
+    while True:
+        sleep(5);
+        print "VAR GLOBAL: " + varGlobal
+        print "VAR TEMP: " + varTemp
+        if varTemp != varGlobal:
+            varTemp = varGlobal
+            data = "Me has llamado a las: " + datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
+            return 'data: {}\n\n'.format(data)
+
+@app.route('/stream3', methods=['GET'])
+def stream3():
+        return Response(streamer3(), mimetype="text/event-stream")
 
 @app.route('/')
 def index():
@@ -73,5 +111,10 @@ if __name__ == '__main__':
     #la opcion adhoc indica a flask que se encargue el 
     #de crear las claves.
     #app.run(debug=True, threaded=True, ssl_context='adhoc')
-    app.run(debug=True)
+    #Es necesario poner threaded=True de forma que se creen varios
+    #hilos y se puedan atender peticiones.
+    #Si no hay hilos, el proceso entrara en el bucle while de streamer3()
+    #y dara "vueltas todo el rato" sin atender a las peticiones que le lleguen
+    #del cliente.
+    app.run(debug=True, threaded=True)
 
