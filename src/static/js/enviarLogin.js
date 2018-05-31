61430cb5c9745f43a5fef18968f1421ca25c5d4f
Pablo <p.pena@edu.uah.es>
2018-05-31 20:04:03 +0200

    En login.html, se ha cambiado la funcionalidad de forma que ahora los datos se envían empleando un form. Sin embargo, la funcionalidad por defecto del form se sobrescribe y se utilizan las funciones utilizadas previamente (enviarLogin.js), las cuales se basan en enviar un post con jQuery con los datos de los campos de texto del formulario e interpretar el json recibido como respuesta del servidor.

--- src/static/js/enviarLogin.js
+++ src/static/js/enviarLogin.js
@@ -18,6 +18,7 @@ function enviarLogin(user, pass)
 function interpretarRespuestaLogin(codigo)
 {
     console.log('Codigo recibido server:' + codigo);
+    //codigo == 1 indica exito, 0 indica fallo en las credenciales.
     return codigo==1;
 }
 
@@ -27,4 +28,39 @@ function mostrarMensajeLogin(codigo)
     {
         window.alert("Credenciales aportadas no válidas");
     }
+}
+
+/*Similar a enviarLogin(), pero en vez de un boton, empleando
+un formulario.
+
+-> el form tiene un atributo que es 'action', donde se indica
+a donde se debe enviar la respuesta. Emplearemos este atributo
+para espcificar la direccion a la que enviaremos el post de JQuery.
+
+->No hace falta que creemos la variable 'datos' (cadena con argumentos),
+ya que podemosobtenerlos directamente con la funcion serialize().
+*/
+//Recibiremos unicamente la cadena identificadora para
+//seleccionar el form con JQuery.
+
+function enviarLoginForm(formSelect)
+{
+    $(formSelect).submit(function(e){
+        //console.log($(this).serialize());//= "user=<usuario>&pass=<password>"
+        $.post($(this).attr('action'),
+        $(this).serialize(),
+        function(respuesta)
+        {
+            //respuestasServidor.js
+            interpretarRespuestaServidor(
+            respuesta,
+            interpretarRespuestaLogin,
+            mostrarMensajeLogin);
+        }, 'json');
+        //Notice that you have to return false from the method that handles the
+        //submit event, otherwise the form will be posted also.
+        //return false;
+        //También puede utilizarse el preventDefault().
+        e.preventDefault();
+    });
 }
